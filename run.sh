33a3ca2415e661ade8564f0aa1ce10c1be550e34
rugarci <rrggcc@gmail.com>
2021-05-02 13:42:40 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
 
-if test -z MAIL_TO; then
-    echo "MAIL_TO is not set"
+if test -z $MAIL_TO; then
+    echo "$MAIL_TO is not set"
     exit 1
 fi
 
-if test -z SMTP_ROOT; then
-    echo "SMTP_ROOT is not set"
+if test -z $SMTP_ROOT; then
+    echo "$SMTP_ROOT is not set"
     exit 1
 fi
 
-if test -z SMTP_HOSTNAME; then
-    echo "SMTP_HOSTNAME is not set"
+if test -z $SMTP_HOSTNAME; then
+    echo "$SMTP_HOSTNAME is not set"
     exit 1
 fi
 
-if test -z SMTP_MAIL_HUB; then
-    echo "SMTP_MAIL_HUB is not set"
+if test -z $SMTP_MAIL_HUB; then
+    echo "$SMTP_MAIL_HUB is not set"
     exit 1
 fi
 
-if test -z SMTP_AUTH_USER; then
-    echo "SMTP_AUTH_USER is not set"
+if test -z $SMTP_AUTH_USER; then
+    echo "$SMTP_AUTH_USER is not set"
     exit 1
 fi
 
-if test -z SMTP_AUTH_PASS; then
-    echo "SMTP_AUTH_PASS is not set"
+if test -z $SMTP_AUTH_PASS; then
+    echo "$SMTP_AUTH_PASS is not set"
     exit 1
 fi
 
-if test -z SMARTD_OPTIONS; then
-    SMARTD_OPTIONS="-n standby"
+if test -z $SMARTD_OPTIONS; then
+    $SMARTD_OPTIONS="-n standby"
 fi
 
 /usr/bin/envsubst < "/etc/smartd.conf.tmpl" > "/etc/smartd.conf"
